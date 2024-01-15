ffddbb4913a9241d84336e8bdddb9eb764dac171
Neftali Yagua <neftaliyagua@fedoraproject.org>
2024-01-15 00:08:14 -0500

    Upload

--- src/app.jsx
+++ src/app.jsx
@@ -21,17 +21,42 @@ import cockpit from 'cockpit';
 import React from 'react';
 import { Alert } from "@patternfly/react-core/dist/esm/components/Alert/index.js";
 import { Card, CardBody, CardTitle } from "@patternfly/react-core/dist/esm/components/Card/index.js";
+import { TabsBoxLight } from './TabsBoxLight';
 
 const _ = cockpit.gettext;
 
 export class Application extends React.Component {
     constructor() {
         super();
-        this.state = { hostname: _("Unknown") };
+        this.state = { 
+            httpd: false,
+            tomcat: false,
+            nginx: false,
+            haproxy: false,
+            webmail: false,
+         };
 
-        cockpit.file('/etc/hostname').watch(content => {
-            this.setState({ hostname: content.trim() });
+         cockpit.file('/etc/httpd/conf/httpd.conf').watch(content => {
+            if(content) {
+                this.setState({ httpd: content.trim() });
+            }
         });
+        cockpit.file('/etc/tomcat/tomcat.conf').watch(content => {
+            if(content) {
+                this.setState({ tomcat: content.trim() });
+            }
+        });
+        cockpit.file('/etc/nginx/nginx.conf').watch(content => {
+            if(content) {
+                this.setState({ nginx: content.trim() });
+            }
+        });
+        cockpit.file('/etc/roundcubemail/config.inc.php').watch(content => {
+            if(content) {
+                this.setState({ webmail: content.trim() });
+            }
+        });
+        
     }
 
     render() {
@@ -39,10 +64,7 @@ export class Application extends React.Component {
             <Card>
                 <CardTitle>Mail</CardTitle>
                 <CardBody>
-                    <Alert
-                        variant="info"
-                        title={ cockpit.format(_("Running on $0"), this.state.hostname) }
-                    />
+                    <TabsBoxLight isEnabled={this.state} />
                 </CardBody>
             </Card>
         );
